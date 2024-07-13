4e4093f81150c1374d64406887fbb1478acf491b
N0rv3ll <norv3ll@icloud.com>
2024-07-13 10:17:36 -0700

    commit 1

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
-// vite.config.js
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
-import adapter from '@sveltejs/adapter-netlify';
-
-export default defineConfig({
-  plugins: [sveltekit()],
-	kit: {
-    adapter: adapter({
-      // if true, will create a Netlify Edge Function rather
-      // than using standard Node.js serverless functions
-      edge: false,
-
-      // if true, will split your app into multiple functions
-      // instead of creating a single one for the entire app.
-      // if `edge` is true, this option cannot be used
-      split: false
-    })
-  }
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var vite_1 = require("@sveltejs/kit/vite");
+var vite_2 = require("vite");
+exports.default = (0, vite_2.defineConfig)({
+    plugins: [(0, vite_1.sveltekit)()]
 });
+//# sourceMappingURL=vite.config.js.map
